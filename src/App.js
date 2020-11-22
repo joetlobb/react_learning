4669687d70b18b960c9f6693248c35d1eadec514
Joetlobb <joetlobb@Joetlobbs-MacBook-Pro.local>
2020-11-22 15:23:49 +0700

    Initial Commit at List and Conditional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,127 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      { id: 1, name: 'Max', age: 28 },
+      { id: 2, name: 'Manu', age: 29 },
+      { id: 3, name: 'Joy', age: 30 }
+    ],
+    otherState: 'some value',
+    showPersons: false
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Joy', age: 30 }
+      ]
+    });
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.splice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  };
+
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              // Can switch between these two, but bind is preferable
+              // click={() => this.deletePersonHandler(index)}
+              click={this.deletePersonHandler.bind(this, index)}
+              name={person.name}
+              age={person.age}
+              key={person.id} />
+          })}
+        </div>
+      )
+    };
+
+    return (
+      <div className="App" >
+        <h1>Hello World!</h1>
+        <p>This is working!</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// import React, { useState } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// import Person from './Person/Person';
+
+// const App = props => {
+//   const [personsState, setPersonsState] = useState({
+//     persons: [
+//       { name: 'Max', age: 28 },
+//       { name: 'Manu', age: 29 },
+//       { name: 'Joy', age: 30 }
+//     ]
+//   });
+
+//   const [otherState, setOtherState] = useState('some value');
+
+//   console.log(personsState, otherState);
+
+//   const switchNameHandler = () => {
+//     // Don't do this
+//     // this.state.persons[0].name = 'NewMax';
+//     setPersonsState({
+//       persons: [
+//         { name: 'NewMax', age: 28 },
+//         { name: 'Manu', age: 29 },
+//         { name: 'Joy', age: 30 }
+//       ],
+//       otherState: personsState.otherState
+//     });
+//   };
+
+//   return (
+//     <div className="App">
+//       <h1>Hello World!</h1>
+//       <p>This is working!</p>
+//       <button onClick={switchNameHandler}>Switch Name</button>
+//       <Person name={personsState.persons[0].name} age={personsState.persons[0].age}></Person>
+//       <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobby is gaming!</Person>
+//       <Person name={personsState.persons[2].name} age={personsState.persons[2].age}></Person>
+//     </div>
+//   );
+
+// }
+
+// export default App;
